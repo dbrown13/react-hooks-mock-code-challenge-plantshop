916ad3ffe6f3b81a0b61e33c81530a2483e0bde0
Deryn Brown <Deryn.2017@gmail.com>
2022-09-29 00:24:31 -0500

    working plantsy

--- src/components/PlantPage.js
+++ src/components/PlantPage.js
@@ -1,14 +1,33 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import NewPlantForm from "./NewPlantForm";
 import PlantList from "./PlantList";
 import Search from "./Search";
 
 function PlantPage() {
+  const [plants, setPlants] = useState([])
+  const [searchTerm, setSearchTerm] = useState("")
+
+  useEffect(() => {
+    fetch("http://localhost:6001/plants")
+    .then((response) => response.json())
+    .then(data => setPlants(data))
+  }, [])
+
+
+  const plantsToDisplay = plants.filter((plant) => {
+    return plant.name.toLowerCase().includes(searchTerm.toLowerCase());
+   });
+
+  
+  const handleAddNewPlant = (newPlant) => {
+    setPlants([...plants, newPlant])
+  }
+
   return (
     <main>
-      <NewPlantForm />
-      <Search />
-      <PlantList />
+      <NewPlantForm handleAddNewPlant={handleAddNewPlant} />
+      <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
+      <PlantList plants={plantsToDisplay}/>
     </main>
   );
 }
